56d4f4523c529c3805f995a0a9a2b688fa65d528
brunogbritto <brunoogbritto@gmail.com>
2023-07-02 23:56:46 -0300

    Product Page style

--- frontend/src/hooks/productHooks.ts
+++ frontend/src/hooks/productHooks.ts
@@ -7,3 +7,10 @@ export const useGetProductsQuery = () =>
     queryKey: ["products"],
     queryFn: async () => (await apiClient.get<Product[]>(`api/products`)).data,
   });
+
+export const useGetProductDetailsBySlugQuery = (slug: string) =>
+  useQuery({
+    queryKey: ["product", slug],
+    queryFn: async () =>
+      (await apiClient.get<Product>(`api/products/slug/${slug}`)).data,
+  });
--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,7 +1,26 @@
 import { Helmet } from "react-helmet-async";
+import { useParams } from "react-router-dom";
+import { useGetProductDetailsBySlugQuery } from "../hooks/productHooks";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { ApiError } from "../types/ApiError";
+import { getError } from "../utils";
 
 export default function ProductPage() {
-  return (
+  const params = useParams();
+  const { slug } = params;
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetProductDetailsBySlugQuery(slug!);
+  return isLoading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">{getError(error as ApiError)}</MessageBox>
+  ) : !product ? (
+    <MessageBox variant="danger">Product Not Found</MessageBox>
+  ) : (
     <div>
       <Helmet>
         <title>Product Page</title>

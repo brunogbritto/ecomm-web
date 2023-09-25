bc8874ed5419b2e2ed2b2056a6af4591b6788813
brunogbritto <brunoogbritto@gmail.com>
2023-09-25 19:54:38 -0300

    order create build added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import { productRouter } from "./routers/productRouter";
 import { seedRouter } from "./routers/seedRouter";
 import { userRouter } from "./routers/userRouter";
+import { orderRouter } from "./routers/orderRouter";
 
 dotenv.config();
 
@@ -34,6 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/seed", seedRouter);
+app.use("/api/orders", orderRouter);
 
 const PORT = 8080;
 app.listen(PORT, () => {

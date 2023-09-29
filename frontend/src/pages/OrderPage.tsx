043a1fb7c61c1e3188e0e5f24aac2ef57e9e855d
brunogbritto <brunoogbritto@gmail.com>
2023-09-28 23:33:23 -0300

    Order page completed

--- frontend/src/pages/OrderPage.tsx
+++ frontend/src/pages/OrderPage.tsx
@@ -1,62 +1,39 @@
-import { useContext, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useParams } from "react-router-dom";
 import { Store } from "../Store";
-import { toast } from "react-toastify";
-import { getError } from "../utils";
+import { useGetOrderDetailsQuery } from "../hooks/orderHooks";
 import { ApiError } from "../types/ApiError";
-import { useCreateOrderMutation } from "../hooks/orderHooks";
-import { Helmet } from "react-helmet-async";
-import CheckoutSteps from "../components/CheckoutSteps";
 import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { getError } from "../utils";
+import { Helmet } from "react-helmet-async";
 
-export default function PlaceOrderPage() {
-  const navigate = useNavigate();
+export default function OrderPage() {
+  const { state } = useContext(Store);
+  const { userInfo } = state;
 
-  const { state, dispatch } = useContext(Store);
-  const { cart, userInfo } = state;
+  const params = useParams();
+  const { id: orderId } = params;
 
-  const round2 = (num: number) => Math.round(num * 100 + Number.EPSILON) / 100;
+  const {
+    data: order,
+    isLoading,
+    error,
+    refetch,
+  } = useGetOrderDetailsQuery(orderId!);
 
-  cart.itemsPrice = round2(
-    cart.cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
-  );
-  cart.shippingPrice = cart.itemsPrice > 100 ? round2(0) : round2(10);
-  cart.taxPrice = round2(0.15 * cart.itemsPrice);
-  cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
-
-  const { mutateAsync: createOrder, isLoading } = useCreateOrderMutation();
-
-  const placeOrderHandler = async () => {
-    try {
-      const data = await createOrder({
-        orderItems: cart.cartItems,
-        shippingAddress: cart.shippingAddress,
-        paymentMethod: cart.paymentMethod,
-        itemsPrice: cart.itemsPrice,
-        shippingPrice: cart.shippingPrice,
-        taxPrice: cart.taxPrice,
-        totalPrice: cart.totalPrice,
-      });
-      dispatch({ type: "CART_CLEAR" });
-      localStorage.removeItem("cartItems");
-      navigate(`/order/${data.order._id}`);
-    } catch (err) {
-      toast.error(getError(err as ApiError));
-    }
-  };
-
-  useEffect(() => {
-    if (!cart.paymentMethod) {
-      navigate("/payment");
-    }
-  }, [cart, navigate]);
-  return (
+  return isLoading ? (
+    <LoadingBox></LoadingBox>
+  ) : error ? (
+    <MessageBox variant="danger">{getError(error as ApiError)}</MessageBox>
+  ) : !order ? (
+    <MessageBox variant="danger">Order Not Found</MessageBox>
+  ) : (
     <div className="max-w-2xl mx-auto p-4">
-      <Helmet>
-        <title>Preview Order | Brechola</title>
-      </Helmet>
-      <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
-      <h1 className="text-2xl font-semibold mt-4 mr-[250px]">Preview Order</h1>
+      <Helmet>Order | Brechola</Helmet>
+      <h1 className="text-2xl font-semibold mt-4 mr-[250px]">
+        Order {orderId}
+      </h1>
       <div className="flex mt-8">
         {/* Coluna da Esquerda */}
         <div className="w-[500px] pr-4">
@@ -65,13 +42,21 @@ export default function PlaceOrderPage() {
             <h2 className="text-xl font-semibold">Shipping Information</h2>
             <div className="bg-white rounded-lg shadow-md p-4 mt-4">
               <strong>Nome: </strong>
-              {cart.shippingAddress.fullName} <br />
+              {order.shippingAddress.fullName} <br />
               <strong>Endereço: </strong>
-              {cart.shippingAddress.address},{cart.shippingAddress.city},{" "}
-              {cart.shippingAddress.postalCode}, {cart.shippingAddress.country}{" "}
-              <br />
-              <div className="mt-1 text-blue-900">
-                <Link to="/shipping">Edit</Link>
+              {order.shippingAddress.address},{order.shippingAddress.city},{" "}
+              {order.shippingAddress.postalCode},{" "}
+              {order.shippingAddress.country} <br />
+              <div>
+                {order.isDelivered ? (
+                  <div className="bg-green-300 border-l-2 py-2 px-4 rounded-lg text-gray-700 font-semibold cursor-not-allowed mt-3">
+                    Objeto entregue em {order.deliveredAt}
+                  </div>
+                ) : (
+                  <div className="bg-yellow-200 border-l-2 py-2 px-4 rounded-lg text-slate-700 font-semibold cursor-not-allowed mt-3">
+                    Seu pedido está em andamento.
+                  </div>
+                )}
               </div>
             </div>
           </div>
@@ -80,7 +65,7 @@ export default function PlaceOrderPage() {
           <div className="mt-8">
             <h2 className="text-xl font-semibold">Payment Information</h2>
             <div className="bg-white rounded-lg shadow-md p-4 mt-4">
-              <strong>Method:</strong> {cart.paymentMethod}
+              <strong>Method:</strong> {order.paymentMethod}
               <div className="mt-1 text-blue-900">
                 <Link to="/payment">Edit</Link>
               </div>
@@ -91,7 +76,7 @@ export default function PlaceOrderPage() {
           <div className="mt-8">
             <h2 className="text-xl font-semibold">Ordered Items</h2>
             <div className="bg-white rounded-lg shadow-md p-4 mt-4">
-              {cart.cartItems.map((item) => (
+              {order.orderItems.map((item) => (
                 <div key={item._id} className="flex mb-4 items-center">
                   <img
                     src={item.image}
@@ -101,9 +86,6 @@ export default function PlaceOrderPage() {
                   <Link to={`/product/${item.slug}`}>{item.name}</Link>
                 </div>
               ))}
-              <div className="text-blue-900">
-                <Link to="/cart">Edit</Link>
-              </div>
             </div>
           </div>
         </div>
@@ -126,16 +108,16 @@ export default function PlaceOrderPage() {
               </ul>
               <ul className="list-none">
                 <li className="flex justify-between items-center ml-5">
-                  $ {cart.itemsPrice.toFixed(2)}
+                  $ {order.itemsPrice.toFixed(2)}
                 </li>
                 <li className="flex justify-between items-center ml-5">
-                  $ {cart.shippingPrice.toFixed(2)}
+                  $ {order.shippingPrice.toFixed(2)}
                 </li>
                 <li className="flex justify-between items-center ml-5">
-                  $ {cart.taxPrice.toFixed(2)}
+                  $ {order.taxPrice.toFixed(2)}
                 </li>
                 <li className="flex justify-between items-center ml-5 mt-2">
-                  <strong>$ {cart.totalPrice.toFixed(2)}</strong>
+                  <strong>$ {order.totalPrice.toFixed(2)}</strong>
                 </li>
               </ul>
             </div>
@@ -143,8 +125,6 @@ export default function PlaceOrderPage() {
               <button
                 type="button"
                 className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-                onClick={placeOrderHandler}
-                disabled={cart.cartItems.length === 0 || isLoading}
               >
                 Place your order
               </button>
